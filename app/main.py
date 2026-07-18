f0f55c70eb9f752646ee6454e3cec6eb3c0c9f50
manisai833-jpg <manisai833@gmail.com>
2026-07-18 06:47:43 +0000

    Fix typo

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
 
-    allow_credentials=Flase,
+    allow_credentials=False,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
